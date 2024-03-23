f506d09aed1c0d433dde568d387adec608f294ca
Redl7C7 <christian_redlich@web.de>
2024-03-23 19:14:47 +0100

    Mel_Specs Preprrocessing Done, Adding CUDA Device Logic

--- FMA_Medium_Data.py
+++ FMA_Medium_Data.py
@@ -3,14 +3,18 @@ import torch
 from torch.utils.data import Dataset
 import pandas as pd
 import torchaudio
-import matplotlib.pyplot as plt
+
+
+# import matplotlib.pyplot as plt
 
 class FreeMusicArchiveMedium(Dataset):
-    def __init__(self, annotations_file, audio_dir, transformation, target_sample_rate):
-        self.annotations = pd.read_csv(annotations_file, delimiter=';')
+    def __init__(self, annotations_file, audio_dir, transformation, target_sample_rate, num_samples, device):
+        self.annotations = pd.read_csv(annotations_file, delimiter=';')  # Semikolon als Trennzeichen
         self.audio_dir = audio_dir
-        self.transformation = transformation
+        self.device = device
+        self.transformation = transformation.to(self.device)
         self.target_sample_rate = target_sample_rate
+        self.num_samples = num_samples
 
     def __len__(self):
         return len(self.annotations)
@@ -22,11 +26,34 @@ class FreeMusicArchiveMedium(Dataset):
         label = self._get_audio_sample_label(index)
         # Beim Laden konvertieren MP3 → WAV
         signal, sr = torchaudio.load(audio_sample_path, format="mp3")
+        if signal is None:
+            raise ValueError("Signal nicht vorhanden, Fehler beim Laden.")
+        signal = signal.to(device)
         # Normalisierungen
+        # gleiche Sample-RATE
         signal = self._resample_if_necessary(signal, sr)
+        # eindimensionale Eingabe (1 Kanal)
         signal = self._mix_down_if_necessary(signal)
+        # Cut, wenn Song zu lang
+        signal = self._cut_if_necessary(signal)
+        # Zero Right Padding für kürzere Songs
+        signal = self._right_pad_if_necessary(signal)
         signal = self.transformation(signal)
         return signal, label
+
+    def _cut_if_necessary(self, signal):
+        # Shape [1] ist beim Tensor-Tupel die Nummer der Samples
+        if signal.shape[1] > self.num_samples:
+            signal = signal[:, :self.num_samples]
+            return signal
+
+    def _right_pad_if_necessary(self, signal):
+        length_signal = signal.shape[1]
+        if length_signal < self.num_samples:
+            num_missing_samples = self.num_samples - length_signal
+            last_dim_padding = (0, num_missing_samples)  # (left Padding Anz, right Padding Anz)
+            signal = torch.nn.functional.pad(signal, last_dim_padding)
+        return signal
 
     def _resample_if_necessary(self, signal, sr):
         if sr != self.target_sample_rate:
@@ -46,7 +73,7 @@ class FreeMusicArchiveMedium(Dataset):
         track_id = self.annotations.loc[index, 'track_id']
         # Formatieren der Track-ID mit führenden Nullen
         filename = str(track_id).zfill(6)
-        # Extrahieren des Segment-Ordners, die ersten drei Zeichen des FIlenames = Name des Unterverzeichnisses
+        # Extrahieren des Segment-Ordners, die ersten drei Zeichen des Filenames = Name des Unterverzeichnisses
         segment_folder = str(filename)[:3]
         # Pfad zum Audiofile erstellen
         path = os.path.join(directory, segment_folder, filename + '.mp3')
@@ -56,27 +83,39 @@ class FreeMusicArchiveMedium(Dataset):
         return self.annotations.iloc[index]["genre_top"]
 
 
-
 if __name__ == "__main__":
     ANNOTATIONS_FILE = 'C:/AI_Datasets/Tracks_Medium.csv'
     AUDIO_DIR = 'C:/AI_Datasets/fma_medium/fma_medium'
-    # NB_AUDIO_SAMPLES = 1321967
-    SAMPLE_RATE = 22500
+    NB_AUDIO_SAMPLES = 1321967
+    SAMPLE_RATE = 44100
 
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+    print(f"Benutze {device} zum berechne.")
+
+    # Transformation/ Vorverarbeitung: Audio in Mel-Spektogramm wandeln
     mel_spectrogram = torchaudio.transforms.MelSpectrogram(
         sample_rate=SAMPLE_RATE,
         n_fft=1024,
         hop_length=512,
-         n_mels=64
+        n_mels=64
     )
 
-    fmamed = FreeMusicArchiveMedium(ANNOTATIONS_FILE, AUDIO_DIR, mel_spectrogram, SAMPLE_RATE)
+    fmamed = FreeMusicArchiveMedium(ANNOTATIONS_FILE,
+                                    AUDIO_DIR,
+                                    mel_spectrogram,
+                                    SAMPLE_RATE,
+                                    NB_AUDIO_SAMPLES,
+                                    device)
 
-
-
+    # Für Versuche:
+    # Beispiel mit Index "2" wählen
+    signal, sr = fmamed[2]
 
     """
-    # Plot des ersten Mel-Spektrogramms
+    # Plot des ersten Mel-Spektrogramms mit oben gewähltem Beispiel
     mel_spec, label = fmamed[12545]
     mel_spec = mel_spec.squeeze(0)  # Reduzieren der Kanaldimension
     plt.figure(figsize=(10, 4))
