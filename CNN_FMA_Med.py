37750181e4e8c271b5500d6d462d4b248f23bb10
Redl7C7 <christian_redlich@web.de>
2024-03-30 14:53:01 +0100

    On and Running mit einem Conv-44 Netz mit schlechter Qualität. Next steps: Optimierung.

--- CNN_FMA_Med.py
+++ CNN_FMA_Med.py
@@ -59,17 +59,22 @@ class CNNetwork(nn.Module):
         # Num Out Chan vorhergehender Layer: 128 | Shape der Eingabe 5 * 4
         # -> Hängt von der Faltung ab, was am Ende reingeht
         # Out Features= 16 -> 16 Genre
-        # Siehe torchsummary: MaxPool2d-12 -> [-1, 128, 5, 4]               0
-        self.linear = nn.Linear(128 * 5 * 4, 16)
+        # Siehe torchsummary: MaxPool2d-12 -> Shape after conv4: torch.Size([9, 128, 5, 163])            0
+        self.linear = nn.Linear(128*5*163, 16)
         # Softmax normaliseren
         self.softmax = nn.Softmax(dim=1)
 
     # Forwarding der Daten zwischen den Schichten
     def forward(self, input_data):
+        # print("Shape before conv1:", input_data.shape)
         x = self.conv1(input_data)
+        # print("Shape after conv1:", x.shape)
         x = self.conv2(x)
+        # print("Shape after conv2:", x.shape)
         x = self.conv3(x)
+        # print("Shape after conv3:", x.shape)
         x = self.conv4(x)
+        # print("Shape after conv4:", x.shape)
         x = self.flatten(x)
         logits = self.linear(x)
         predictions = self.softmax(logits)
