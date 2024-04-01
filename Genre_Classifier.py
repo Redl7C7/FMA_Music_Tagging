2ae96c972ed9153bbc51ab5f63953a76924edbed
Redl7C7 <christian_redlich@web.de>
2024-04-01 14:42:10 +0200

    BATCH_SIZE 24

--- Genre_Classifier.py
+++ Genre_Classifier.py
@@ -12,7 +12,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
     average_precision_score
 
 # Konstanten
-BATCH_SIZE = 32
+BATCH_SIZE = 24
 EPOCHS = 2
 LEARNING_RATE = 0.001
 ANNOTATIONS_FILE = 'C:/AI_Datasets/Tracks_Medium.csv'
