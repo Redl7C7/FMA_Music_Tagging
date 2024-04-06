0b3315850190856d94a5f4fdca50d6534726ed06
Redl7C7 <christian_redlich@web.de>
2024-04-06 19:17:47 +0200

    Rollback auf heute Mittag.

--- Genre_Classifier.py
+++ Genre_Classifier.py
@@ -1,12 +1,9 @@
 import random
-
 import torch
-import torchmetrics
 import torchvision.models as models
 import torchvision.transforms as transforms
 from torch import nn
 from torch.utils.data import DataLoader
-from torchvision.models import VGG19_Weights
 from tqdm import tqdm
 
 from FMA_Medium_MelSpecs import FreeMusicArchiveMedium
@@ -43,34 +40,15 @@ def split_data(dataset, train_percent=0.01, val_percent=0.025, test_percent=0.02
     return train_data, val_data, test_data
 
 
-def compute_metrics(y_true, y_pred):
-    y_true_tensor = torch.tensor(y_true, dtype=torch.float32)
-    y_pred_tensor = torch.tensor(y_pred, dtype=torch.float32)
-
-    # Berechne die Metriken
-    acc = torchmetrics.functional.accuracy(y_pred_tensor, y_true_tensor, task='multiclass')  # Angabe des task-Parameters
-
-    prec = torchmetrics.functional.precision(y_pred_tensor, y_true_tensor, average='macro', task='multiclass')
-    rec = torchmetrics.functional.recall(y_pred_tensor, y_true_tensor, average='macro', task='multiclass')
-    f1_score = (torchmetrics.functional.f1_score(y_pred_tensor, y_true_tensor, average='macro', task='multiclass'))
-    roc_auc_score = (torchmetrics.functional.auroc(y_pred_tensor, y_true_tensor, average='macro', task='multiclass'))
-    pr_auc_score = torchmetrics.functional.average_precision(y_pred_tensor, y_true_tensor, average='macro', task='multiclass')
-
-    return acc.item(), prec.item(), rec.item(), f1_score.item(), roc_auc_score.item(), pr_auc_score.item()
-
-
-def create_data_loader(train_data, batch_size):
-    train_dataloader = DataLoader(train_data, batch_size=batch_size)
-    return train_dataloader
+def create_data_loader(data, batch_size):
+    data_loader = DataLoader(data, batch_size=batch_size)
+    return data_loader
 
 
 def train_single_epoch(model, data_loader, loss_fn, optimiser, device):
     model.train()  # Setze Modell in den Trainingsmodus
     running_loss = 0.0
-    correct_predictions = 0
     total_samples = 0
-    y_true = []
-    y_pred = []
     with tqdm(total=len(data_loader), desc="Epoch Training") as pbar:
         for inputs, targets in data_loader:
             inputs = inputs.to(device)
@@ -79,152 +57,61 @@ def train_single_epoch(model, data_loader, loss_fn, optimiser, device):
             # Berechnen der Vorhersagen und des Verlusts
             outputs = model(inputs)
             loss = loss_fn(outputs, targets)
-            # Ausgabe der Labels und Vorhersagen
-            print("Labels:", targets)
-            print("Vorhersagen:", outputs)
 
             # Backpropagation und Optimierung
             optimiser.zero_grad()
             loss.backward()
             optimiser.step()
 
-            # Berechne die Genauigkeit
-            _, predicted = torch.max(outputs, 1)
-            predicted = predicted.to(device)
-            correct_predictions += torch.sum((predicted == targets).int()).item()
-            total_samples += targets.size(0)
-
             # Verfolgen des Verlusts für die Ausgabe
             running_loss += loss.item() * inputs.size(0)
-
-            # Verfolgen der Vorhersagen für Metriken
-            y_true.extend(targets.tolist())
-            y_pred.extend(predicted.tolist())
 
             # Fortschrittsanzeige
             pbar.update(1)
             pbar.set_postfix({'loss': running_loss / total_samples})
 
-        # Berechnen der Durchschnittsverlust und der Genauigkeit für die Epoche
-        epoch_loss = running_loss / len(data_loader.dataset)
-        epoch_accuracy = correct_predictions / total_samples
+    # Berechnen des Durchschnittsverlusts für die Epoche
+    epoch_loss = running_loss / len(data_loader.dataset)
 
-        # Berechnen der Metriken
-        accuracy, precision, recall, f1, pr_auc, roc_auc = compute_metrics(y_true, y_pred)
-
-        # Ausgabe von Verlust und Metriken
-        print(f"Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.4f}, "
-              f"Precision: {precision:.4f}, Recall: {recall:.4f}, F1-Score: {f1:.4f}, "
-              f"PR-AUC: {pr_auc:.4f}, ROC-AUC: {roc_auc:.4f}")
-
-        return epoch_loss, epoch_accuracy, accuracy, precision, recall, f1, pr_auc
+    return epoch_loss
 
 
 def validate(model, data_loader, loss_fn, device):
     model.eval()  # Setze das Modell in den Evaluierungsmodus
     running_loss = 0.0
-    correct_predictions = 0
     total_samples = 0
-    y_true = []
-    y_pred = []
     with torch.no_grad():
         with tqdm(total=len(data_loader), desc="Validation") as pbar:
             for inputs, targets in data_loader:
                 inputs = inputs.to(device)
                 targets = targets.to(device)
 
-                # Berechne die Vorhersagen und den Verlust
+                # Berechnen der Vorhersagen und des Verlusts
                 outputs = model(inputs)
                 loss = loss_fn(outputs, targets)
 
-                # Berechne die Genauigkeit
-                _, predicted = torch.max(outputs, 1)
-                correct_predictions += torch.sum((predicted == targets).int()).item()
-                total_samples += targets.size(0)
-
-                # Verfolge den Verlust für die Ausgabe
+                # Verfolgen des Verlusts für die Ausgabe
                 running_loss += loss.item() * inputs.size(0)
-
-                # Verfolge die Vorhersagen für Metriken
-                y_true.extend(targets.numpy())
-                y_pred.extend(predicted.numpy())
 
                 # Fortschrittsanzeige
                 pbar.update(1)
                 pbar.set_postfix({'loss': running_loss / total_samples})
 
-            # Berechne den Durchschnittsverlust und die Genauigkeit für die Validierung
-            epoch_loss = running_loss / len(data_loader.dataset)
-            epoch_accuracy = correct_predictions / total_samples
+        # Berechnen des Durchschnittsverlusts für die Validierung
+        epoch_loss = running_loss / len(data_loader.dataset)
 
-            # Berechne die Metriken
-            accuracy, precision, recall, f1, pr_auc, roc_auc = compute_metrics(y_true, y_pred)
-
-            return epoch_loss, epoch_accuracy, accuracy, precision, recall, f1, pr_auc
+        return epoch_loss
 
 
 def train(model, train_data_loader, val_data_loader, loss_fn, optimiser, device, epochs):
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1}/{epochs}")
-        train_loss, train_accuracy = train_single_epoch(model, train_data_loader, loss_fn, optimiser,
-                                                                       device)
+        train_loss = train_single_epoch(model, train_data_loader, loss_fn, optimiser, device)
         print("---------------------------")
-        v_loss, v_accuracy, v_precision, v_recall, v_f1, v_pr_auc, v_roc_auc = validate(model, val_data_loader, loss_fn,
-                                                                                        device)
-        print(f"\nValidation Loss: {v_loss:.4f}, Validation Accuracy: {v_accuracy:.4f}, "
-              f"Validation Precision: {v_precision:.4f}, Validation Recall: {v_recall:.4f}, "
-              f"Validation F1-Score: {v_f1:.4f}, Validation PR-AUC: {v_pr_auc:.4f}, "
-              f"Validierung ROC-AUC: {v_roc_auc:.4f}")
+        v_loss = validate(model, val_data_loader, loss_fn, device)
+        print(f"\nValidation Loss: {v_loss:.4f}")
         print("---------------------------")
     print("Training beendet.")
-
-
-def test(model, test_data_loader, loss_fn, device):
-    model.eval()  # Setze das Modell in den Evaluierungsmodus
-    running_loss = 0.0
-    correct_predictions = 0
-    total_samples = 0
-    y_true = []
-    y_pred = []
-    with torch.no_grad():
-        with tqdm(total=len(test_data_loader), desc="Testing") as pbar:
-            for inputs, targets in test_data_loader:
-                inputs = inputs.to(device)
-                targets = targets.to(device)
-
-                # Berechne die Vorhersagen und den Verlust
-                outputs = model(inputs)
-                loss = loss_fn(outputs, targets)
-
-                # Berechne die Genauigkeit
-                _, predicted = torch.max(outputs, 1)
-                correct_predictions += torch.sum((predicted == targets).int()).item()
-                total_samples += targets.size(0)
-
-                # Verfolge den Verlust für die Ausgabe
-                running_loss += loss.item() * inputs.size(0)
-
-                # Verfolge die Vorhersagen für Metriken
-                y_true.extend(targets.cpu().numpy())
-                y_pred.extend(predicted.cpu().numpy())
-
-                # Fortschrittsanzeige
-                pbar.update(1)
-                pbar.set_postfix({'loss': running_loss / total_samples})
-
-            # Berechne den Durchschnittsverlust und die Genauigkeit für den Test
-            test_loss = running_loss / len(test_data_loader.dataset)
-            test_accuracy = correct_predictions / total_samples
-
-            # Berechne die Metriken
-            accuracy, precision, recall, f1, pr_auc, roc_auc = compute_metrics(y_true, y_pred)
-
-            # Gib den Verlust und die Metriken aus
-            print(f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f}, "
-                  f"Test Precision: {precision:.4f}, Test Recall: {recall:.4f}, "
-                  f"Test F1-Score: {f1:.4f}, Test PR-AUC: {pr_auc:.4f}")
-
-            return test_loss, test_accuracy
 
 
 if __name__ == "__main__":
@@ -234,6 +121,7 @@ if __name__ == "__main__":
     else:
         device = "cpu"
     print(f"Using {device}")
+
     # Datensatzklasse instanziieren
     print(f"Starte mit folgenden Params:"
           f"BATCH_SIZE = {BATCH_SIZE}",
@@ -262,43 +150,26 @@ if __name__ == "__main__":
     train_dataloader = create_data_loader(train_data, batch_size=BATCH_SIZE)
     print("Lade Validierungsdaten.")
     val_dataloader = create_data_loader(val_data, batch_size=BATCH_SIZE)
-    # test_dataloader = create_data_loader(test_data, batch_size=BATCH_SIZE)
 
     # Nutzen des vorgestalteten Pytorch VGG19
     print("vgg19 erstellen.")
-    VGG19 = models.vgg19(weights=VGG19_Weights.DEFAULT).to(device)
+    VGG19 = models.vgg19(pretrained=True)
     num_classes = 16
     VGG19.classifier[6] = nn.Linear(4096, num_classes)
-
-    # Optional: Hinzufügen einer Softmax-Schicht, falls die Ausgänge als Wahrscheinlichkeiten interpretiert werden sollen
-    # vgg19.classifier.add_module('7', nn.Softmax(dim=1))
-
+    VGG19.to(device)
     # Ausgabe des angepassten VGG19-Modells
     print(VGG19)
-    # VGG19 = models.vgg19(weights=None).to(device)
-    # Die Eingabeschicht des VGG19-Modells ändern, um mit den Spektrogramm-Eingabedaten umzugehen
+
+    # Eingang des VGG19-Modells anpassen, um mit den Spektrogramm-Eingabedaten umzugehen
     print("Eingang des VGG19 auf Spektogramme in Tensor anpassen.")
-    # VGG19.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)).to(device)
-    # initialisiere loss function + optimiser
+
+    # Initialisiere Loss-Funktion + Optimiser
     loss_fn = nn.CrossEntropyLoss()
     optimiser = torch.optim.Adam(VGG19.parameters(), lr=LEARNING_RATE)
-    # train model
+
+    # Trainiere das Modell
     train(VGG19, train_dataloader, val_dataloader, loss_fn, optimiser, device, EPOCHS)
-    # save model
+
+    # Speichere das trainierte Modell
     torch.save(VGG19.state_dict(), "VGG19_fma_med.pth")
     print("Trainiertes Netz als cnn_fma_med.pth gespeichert.")
-    print("Lade Testdaten.")
-    test_dataloader = create_data_loader(test_data, batch_size=BATCH_SIZE)
-    # Testen des Modells
-    test_loss, test_accuracy = test(VGG19, test_dataloader, loss_fn, device)
-    # Modell erzeugen und CUDA zuordnen
-    """
-    # cnn = CNNetwork().to(device)
-    VGG19 = VGG(
-        in_channels=1,
-        in_height=224,
-        in_width=224,
-        architecture=VGG_types["VGG19"]
-    ).to(device)
-    print(VGG19)
-    """
