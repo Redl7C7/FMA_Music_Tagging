d0c5f297c4a6ce9795ce0dae66a5a1af7e884062
Redl7C7 <123874497+Redl7C7@users.noreply.github.com>
2024-04-12 18:45:07 +0200

    MFCCs 224x224 erzeugt

--- mfccs_imagegenerator.py
+++ mfccs_imagegenerator.py
@@ -6,8 +6,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-cep_lifter = 22
-
+cep_lifter = 50
+N_MFCC = 13
+N_FTT = 2048
+HOP_LENGTH = 512
+N_MELS = 128
 if __name__ == "__main__":
     # Verzeichnis, in das die Bilder gespeichert werden sollen
     wav_directory = "C:/AI_Datasets/fma_medium/wav"
@@ -21,18 +24,22 @@ if __name__ == "__main__":
     annotations = pd.read_csv(annotations_file, delimiter=';')  # Semikolon als Trennzeichen anpassen
 
     # Transformation für MFCCs
-    mfcc_transform = transforms.MFCC(sample_rate=44100, n_mfcc=13)
+    mfcc_transform = transforms.MFCC(sample_rate=44100, n_mfcc=N_MFCC, melkwargs={
+        "n_fft": N_FTT,
+        "n_mels": N_MELS,
+        "hop_length": HOP_LENGTH,
+        "mel_scale": "htk",
+    },)
     # Schleife über alle Zeilen in der CSV
     for index, row in annotations.iterrows():
         # Extrahiere den Dateinamen aus der entsprechenden Spalte der Zeile
-        filenum = row['Filename']  # Hier musst du den Namen der Spalte anpassen, die den Dateinamen enthält
+        filenum = row['WAV-name']  # Hier musst du den Namen der Spalte anpassen, die den Dateinamen enthält
         filename = str(filenum).zfill(10)
         segment_folder = str(filename)[:3]
         # Konstruieren des vollständigen Pfads zur Wave-Datei
         wav_path = os.path.join(wav_directory, filename[:-4] + '.wav')
         # Konstruieren des vollständigen Pfads zum Zielbild
         image_path = os.path.join(image_directory, filename[:-4] + '.png')
-        cep_lifter= 22
         # Überprüfen, ob das Bild bereits existiert
         if os.path.exists(image_path):
             print(f"Bild {image_path} existiert bereits, überspringe Konvertierung.")
@@ -41,28 +48,37 @@ if __name__ == "__main__":
 
         # Lade die Wave-Datei
         waveform, sample_rate = torchaudio.load(wav_path)
-        print(f"{waveform.shape}")
         # Überprüfen, ob die Waveform mono ist
         if waveform.shape[0] > 1:
             # Falls die Waveform mehr als einen Kanal hat, wähle den ersten Kanal aus
             waveform = waveform[0]
-        print(f"{waveform.shape}")
         # Erzeuge die MFCCs und füge eine Batch-Dimension hinzu
         mfcc = mfcc_transform(waveform).squeeze().detach().numpy()
-        print(f"{mfcc.shape}")
         # Sinusförmiges Anheben der MFCCs
         nframes, ncoeff = mfcc.shape
         n = np.arange(ncoeff)
         lift = 1 + (cep_lifter / 2) * np.sin(np.pi * n / cep_lifter)
         mfcc *= lift
-
-        # Konvertiere MFCCs in ein Bildformat (z.B. als Graustufenbild)
-        mfcc_image = np.uint8((mfcc - mfcc.min()) / (mfcc.max() - mfcc.min()) * 255)
-
-        # Erzeuge ein PIL-Image-Objekt
-        mfcc_image = Image.fromarray(mfcc_image)
-
-        # Speichere das Bild
-        mfcc_image.save(image_path)
+        """
+        # Plotte das MFCC-Diagramm
+        duration = waveform.size(1) / sample_rate
+        plt.figure(figsize=(10, 4))
+        # plt.imshow(mfcc.T, origin='lower', aspect='auto', cmap='viridis')
+        plt.imshow(mfcc, origin='lower', aspect='auto', cmap='viridis',
+                   extent=[0, duration, 0, N_MFCC])
+        plt.colorbar(label='Amplitude')
+        plt.xlabel('Zeit')
+        plt.ylabel('MFCC Koeffizienten')
+        plt.title(f'MFCC-Diagramm von {filename}')
+        plt.show()
+        """
+        plt.figure(figsize=(2.9, 2.92))
+        duration = waveform.size(1) / sample_rate
+        # plt.figure(figsize=(224/100, 224/100))
+        plt.imshow(mfcc, origin='lower', aspect='auto', cmap='viridis', extent=[0, duration, 0, N_MFCC])
+        plt.axis('off')
+        # plt.savefig(image_path)  # Speichern des Diagramms als PNG
+        plt.savefig(image_path, bbox_inches='tight', pad_inches=0)
+        plt.close()
 
         print(f"Bild {filename[:-4]}.png gespeichert unter {image_path}")
