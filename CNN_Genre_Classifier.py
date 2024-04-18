346e5ccc4da5dd3d3be14fd0728419b5369b2435
Redl7C7 <123874497+Redl7C7@users.noreply.github.com>
2024-04-18 06:35:15 +0200

    kleiner clssifier

--- CNN_Genre_Classifier.py
+++ CNN_Genre_Classifier.py
@@ -20,9 +20,9 @@ from sklearn.preprocessing import OneHotEncoder
 # Konstanten
 BATCH_SIZE = 128
 EPOCHS = 100
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.0001
 # L2-Regulierung / Norm-Penalisierung
-WEIGHT_DECAY = 0.01
+WEIGHT_DECAY = 0.0001
 FREEZE = True
 ANNOTATIONS_FILE = 'C:/AI_Datasets/Tracks_Medium.csv'
 MFCC_IMAGE_DIR = "C:/AI_Datasets/fma_medium/bunt-mfcc-images/"
